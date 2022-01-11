ff324ad42a6e704d5bebe92a5fe46c0fdeb15b0e
fcondori1 <frankcondori97@gmail.com>
2022-01-10 21:42:50 -0500

    main page update

--- golazo-highlights-app/src/components/Main/Main.js
+++ golazo-highlights-app/src/components/Main/Main.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './Main.css'
 
 function Main(props) {
+	const [matches, setMatches] = useState([]);
+	useEffect(() => {
+		axios
+			.get('https://www.scorebat.com/video-api/v3/')
+			.then((res) => setMatches(res.data.response));
+	}, []);
+	// function getRandomGame(e) {
+	// 	return Math.floor(Math.random() * matches.response.length);
+	// }
 	return (
 		<div className='page'>
-			<p>Hello from the main page Welcome to the games of the week!</p>
-			{/* <img src='../../../public/soccerfield2.png' alt='soccer field' className='soccer'></img> */}
-			
+			<p>Welcome to the Golazo(Goal) App!</p>
+			<p>Go to Recent Matches to see the lates games of the week and wait for updates to come for a better watching experience!</p>
+			<div className='stadium'></div>
 		</div>
 	);
 }
